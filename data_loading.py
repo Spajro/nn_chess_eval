c1b900ccfb16788472f727c81b0ae0caf35d2102
Spajro <rafal.spyra@onet.pl>
2024-10-24 00:47:49 +0200

    rename actual data loader to 3d data loader

--- data_loading.py
+++ data_loading.py
@@ -3,28 +3,28 @@ import chess
 import torch
 
 
-def bitboard_to_tensor(bitboard: int) -> torch.Tensor:
+def bitboard_to_tensor_3d(bitboard: int) -> torch.Tensor:
     li = [1.0 if digit == '1' else 0.0 for digit in bin(bitboard)[2:]]
     li = [0.0 for _ in range(64 - len(li))] + li
     return torch.tensor(li).reshape((8, 8))
 
 
-def fen_to_tensor(fen: str) -> [torch.Tensor]:
+def fen_to_tensor_3d(fen: str) -> [torch.Tensor]:
     board = chess.Board(fen)
     return torch.stack([
-        bitboard_to_tensor(board.occupied_co[chess.WHITE]),
-        bitboard_to_tensor(board.occupied_co[chess.BLACK]),
-        bitboard_to_tensor(board.pawns),
-        bitboard_to_tensor(board.kings),
-        bitboard_to_tensor(board.queens),
-        bitboard_to_tensor(board.knights),
-        bitboard_to_tensor(board.bishops),
-        bitboard_to_tensor(board.rooks)
+        bitboard_to_tensor_3d(board.occupied_co[chess.WHITE]),
+        bitboard_to_tensor_3d(board.occupied_co[chess.BLACK]),
+        bitboard_to_tensor_3d(board.pawns),
+        bitboard_to_tensor_3d(board.kings),
+        bitboard_to_tensor_3d(board.queens),
+        bitboard_to_tensor_3d(board.knights),
+        bitboard_to_tensor_3d(board.bishops),
+        bitboard_to_tensor_3d(board.rooks)
     ])
 
 
-def data_to_tensors(data: (str, float)) -> (torch.Tensor, torch.Tensor):
-    return [(fen_to_tensor(fen), torch.tensor(value / 100, dtype=torch.float)) for fen, value in data]
+def data_to_tensors_3d(data: (str, float)) -> (torch.Tensor, torch.Tensor):
+    return [(fen_to_tensor_3d(fen), torch.tensor(value / 100, dtype=torch.float)) for fen, value in data]
 
 
 def load_data_from_file(file_path: str) -> [(str, float)]:
@@ -49,5 +49,5 @@ def dataset_to_batches(dataset: [(torch.Tensor, torch.Tensor)], batch_size) -> [
     return batches
 
 
-def load_dataset(file_path: str, batch_size) -> [(torch.Tensor, torch.Tensor)]:
-    return dataset_to_batches(data_to_tensors(load_data_from_file(file_path)), batch_size)
+def load_dataset_3d(file_path: str, batch_size) -> [(torch.Tensor, torch.Tensor)]:
+    return dataset_to_batches(data_to_tensors_3d(load_data_from_file(file_path)), batch_size)
