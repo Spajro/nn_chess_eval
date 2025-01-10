62075a935bd82e581db6095442be7459b5d9a2fe
Spajro <rafal.spyra@onet.pl>
2025-01-10 18:05:48 +0100

    add checkpoint saving

--- src/core.py
+++ src/core.py
@@ -3,11 +3,21 @@ import time
 import torch
 
 
-def train(train_data, test_data, model, criterion, optimizer, accuracy, epoch):
+def train(train_data, test_data, model, criterion, optimizer, accuracy, epoch, checkpoint=None):
+    if checkpoint:
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        start = checkpoint['epoch']
+        for i, passed_time, train_loss, train_acc, val_loss, val_acc, test_loss, test_acc in checkpoint['history']:
+            print(
+                f"Epoch [{i}/{epoch}],  train: {train_loss:.5f},{train_acc:.1f}    san_check: {val_loss:.5f}, {val_acc:.1f}  test: {test_loss:.5f},{test_acc:.1f},  time: {passed_time / 1000}s")
+        print("Checkpoint loaded")
+    else:
+        start = 0
     model.train(True)
     model.cuda()
     criterion.cuda()
-    for i in range(epoch):
+    for i in range(start, epoch):
         time_started = time.time() * 1000
         loss_sum = 0.0
         accuracy_sum = 0.0
@@ -22,15 +32,21 @@ def train(train_data, test_data, model, criterion, optimizer, accuracy, epoch):
             loss_sum += loss.item()
             accuracy_sum += accuracy_value.item()
 
-        loss_average = loss_sum / len(train_data)
-        accuracy_average = accuracy_sum / len(train_data)
+        train_loss = loss_sum / len(train_data)
+        train_acc = accuracy_sum / len(train_data)
 
         test_loss, test_acc = iterate(test_data, model, criterion, accuracy)
         val_loss, val_acc = iterate(train_data, model, criterion, accuracy)
 
         passed_time = math.ceil(time.time() * 1000 - time_started)
+        checkpoint['history'].append(
+            [i + 1, passed_time / 1000, train_loss, train_acc, val_loss, val_acc, test_loss, test_acc])
+        checkpoint = {'epoch': i+1,
+                      'model': model.state_dict(),
+                      'optimizer': optimizer.state_dict(),
+                      'history': checkpoint['history']}
         print(
-            f"Epoch [{i + 1}/{epoch}],  train: {loss_average:.5f},{accuracy_average:.1f}    san_check: {val_loss:.5f}, {val_acc:.1f}  test: {test_loss:.5f},{test_acc:.1f},  time: {passed_time / 1000}s")
+            f"Epoch [{i + 1}/{epoch}],  train: {train_loss:.5f},{train_acc:.1f}    san_check: {val_loss:.5f}, {val_acc:.1f}  test: {test_loss:.5f},{test_acc:.1f},  time: {passed_time / 1000}s")
 
 
 def test(data, model, criterion, accuracy):
