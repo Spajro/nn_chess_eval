20e808a4fbcbf6908f85b794a7ab88f802ed0238
Spajro <rafal.spyra@onet.pl>
2025-01-10 19:17:40 +0100

    separate log method in core

--- src/core.py
+++ src/core.py
@@ -9,8 +9,9 @@ def train(train_data, test_data, model, criterion, optimizer, accuracy, epoch, c
         optimizer.load_state_dict(checkpoint['optimizer'])
         start = checkpoint['epoch']
         for i, passed_time, train_loss, train_acc, val_loss, val_acc, test_loss, test_acc in checkpoint['history']:
-            print(
-                f"Epoch [{i}/{epoch}],  train: {train_loss:.5f},{train_acc:.1f}    san_check: {val_loss:.5f}, {val_acc:.1f}  test: {test_loss:.5f},{test_acc:.1f},  time: {passed_time / 1000}s")
+            log([('train', train_loss, train_acc), ('san', val_loss, val_acc), ('test', test_loss, test_acc)],
+                passed_time,
+                (i, epoch))
         print("Checkpoint loaded")
     else:
         start = 0
@@ -41,12 +42,13 @@ def train(train_data, test_data, model, criterion, optimizer, accuracy, epoch, c
         passed_time = math.ceil(time.time() * 1000 - time_started)
         checkpoint['history'].append(
             [i + 1, passed_time / 1000, train_loss, train_acc, val_loss, val_acc, test_loss, test_acc])
-        checkpoint = {'epoch': i+1,
+        checkpoint = {'epoch': i + 1,
                       'model': model.state_dict(),
                       'optimizer': optimizer.state_dict(),
                       'history': checkpoint['history']}
-        print(
-            f"Epoch [{i + 1}/{epoch}],  train: {train_loss:.5f},{train_acc:.1f}    san_check: {val_loss:.5f}, {val_acc:.1f}  test: {test_loss:.5f},{test_acc:.1f},  time: {passed_time / 1000}s")
+        log([('train', train_loss, train_acc), ('san', val_loss, val_acc), ('test', test_loss, test_acc)],
+            passed_time / 1000,
+            (i, epoch))
 
 
 def test(data, model, criterion, accuracy):
@@ -56,8 +58,7 @@ def test(data, model, criterion, accuracy):
     time_started = time.time() * 1000
     loss_average, accuracy_average = iterate(data, model, criterion, accuracy)
     passed_time = math.ceil(time.time() * 1000 - time_started)
-    print(
-        f"test_loss: {loss_average}, test_accuracy: {accuracy_average}, time: {passed_time / 1000}s")
+    log([('test', loss_average, accuracy_average)], passed_time / 1000)
 
 
 def iterate(data, model, criterion, accuracy):
@@ -75,3 +76,13 @@ def iterate(data, model, criterion, accuracy):
     loss_average = loss_sum / len(data)
     accuracy_average = accuracy_sum / len(data)
     return loss_average, accuracy_average
+
+
+def log(data: (str, float, float), passed_time: float, epoch: (int, int) = None):
+    result = ""
+    if epoch:
+        result += f"Epoch [{epoch[0]}/{epoch[1]}], "
+    for text, loss, acc in data:
+        result += f"{text}: {loss:.5f} {acc:.2f}, "
+    result += f" time: {passed_time:.2f}s"
+    print(result)
