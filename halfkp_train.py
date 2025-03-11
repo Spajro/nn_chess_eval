3983623481441c50a3ad32c2d1dcbfd811757f13
Spajro <rafal.spyra@onet.pl>
2025-03-11 23:24:58 +0100

    add argparse usage to halfkp_train

--- halfkp_train.py
+++ halfkp_train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 from torch import nn
 
@@ -31,20 +33,37 @@ def accuracy(out, truth):
     return wdl_to_cp(torch.abs(truth - out))
 
 
-BATCH_SIZE = 64
-DEVICE = "cuda:0"
+parser = argparse.ArgumentParser(description='Halfkp NNUE training')
+parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+parser.add_argument('--epochs', type=int, default=300, help='number of epochs')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+parser.add_argument('--prefix', type=str, default='halfkp_train', help='checkpoint prefix')
+parser.add_argument('--device', type=str, default='cpu', help='cuda:X or cpu')
+parser.add_argument('--cp-interval', type=int, default=25, help='checkpoint interval')
+parser.add_argument('--load-cp', type=str, help='name of checkpoint to load')
+parser.add_argument('--san-check', action='store_true', help='test on training set after every epoch')
+args = parser.parse_args()
 
-train_dataset = HalfKpDataset(TRAIN_DATASET_PATCH, BATCH_SIZE, DEVICE)
-test_dataset = HalfKpDataset(TEST_DATASET_PATCH, BATCH_SIZE, DEVICE)
+lr = args.lr
+epochs = args.epochs
+batch_size = args.batch_size
+prefix = args.prefix
+device = torch.device(args.device)
+cp_interval = args.cp_interval
+load_cp = args.load_cp
+san_check = args.san_check
+
+train_dataset = HalfKpDataset(TRAIN_DATASET_PATCH, batch_size, device)
+test_dataset = HalfKpDataset(TEST_DATASET_PATCH, batch_size, device)
 len(train_dataset), len(test_dataset)
 
 model = Model()
-optimizer = torch.optim.SGD(model.classifier.parameters(), lr=0.001)
+optimizer = torch.optim.SGD(model.classifier.parameters(), lr=lr)
 
 LOAD_FLAG = False
 
-if LOAD_FLAG:
-    checkpoint = torch.load('halfkp_checkpoint.pt')
+if load_cp:
+    checkpoint = torch.load(load_cp)
 else:
     checkpoint = {'epoch': 0,
                   'model': model.state_dict(),
@@ -57,14 +76,16 @@ train(train_dataset,
       nn.MSELoss(),
       optimizer,
       accuracy,
-      300,
-      DEVICE,
+      epochs,
+      device,
       checkpoint=checkpoint,
-      prefix="halfkp")
+      save_checkpoint_every=cp_interval,
+      prefix=prefix,
+      san_check=san_check)
 
-torch.save(checkpoint, 'halfkp_final.pt')
+torch.save(checkpoint, prefix + '-final.pt')
 
 torch.save([[model.layer1.weight.tolist(), model.layer1.bias.tolist()],
             [model.layer2.weight.tolist(), model.layer2.bias.tolist()],
             [model.layer3.weight.tolist(), model.layer3.bias.tolist()]],
-           "halfkp_wb.pt")
+           prefix + '-wb.pt')
