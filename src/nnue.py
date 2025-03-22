486ce9ca1ed4c285eb347252c915f01ab287a425
Spajro <rafal.spyra@onet.pl>
2025-03-22 21:27:32 +0100

    change nnue model

--- src/nnue.py
+++ src/nnue.py
@@ -1,23 +1,32 @@
+import torch
 from torch import nn
 
 from src.data_loading_halfkp import FEATURES_COUNT
 
-x0 = 2 * FEATURES_COUNT
-x1 = 2 ** 8
-x2 = 2 ** 5
+M = 2 ** 7
+N = 2 ** 5
 
 
 class NNUE(nn.Module):
     def __init__(self):
         super(NNUE, self).__init__()
-        self.layer1 = nn.Linear(x0, x1)
-        self.layer2 = nn.Linear(x1, x2)
-        self.layer3 = nn.Linear(x2, 1)
+        self.layer1 = nn.Linear(FEATURES_COUNT, M)
+        self.layer2 = nn.Linear(2 * M, N)
+        self.layer3 = nn.Linear(N, 1)
         self.classifier = nn.Sequential(self.layer1,
-                                        nn.ReLU(),
                                         self.layer2,
-                                        nn.ReLU(),
                                         self.layer3)
 
     def forward(self, X):
-        return self.classifier.forward(X)
+        white = X[:, :FEATURES_COUNT]
+        black = X[:, FEATURES_COUNT:]
+
+        w = self.layer1(white)
+        b = self.layer1(black)
+
+        # accumulator = (color * torch.cat([w, b], dim=1)) + ((1 - color) * torch.cat([b, w], dim=1))
+        accumulator = (torch.cat([w, b], dim=1))
+
+        relu1 = torch.clamp(accumulator, 0.0, 1.0)
+        relu2 = torch.clamp(self.layer2(relu1), 0.0, 1.0)
+        return self.layer3.forward(relu2)
