b950c5e524bc80464758ea84c6fc3ce47ff6d957
Spajro <rafal.spyra@onet.pl>
2025-03-28 13:43:07 +0100

    add uneven dataset split

--- dataset_split.py
+++ dataset_split.py
@@ -5,28 +5,14 @@ from src.loading.data_loading import save_dataset_to_csv
 from src.patches import GAMES_DATASET_PATCH, TEST_DATASET_PATCH, TRAIN_DATASET_PATCH
 
 games_dataset = load_data_from_file(GAMES_DATASET_PATCH)
-even = []
-odd = []
-for fen, value in games_dataset:
-    if abs(value) <= 100:
-        even.append((fen, value))
-    else:
-        odd.append((fen, value))
-
-print("Even:", len(even), " Odd: ", len(odd))
-if len(even) < len(odd):
-    odd = odd[:len(even)]
-elif len(even) > len(odd):
-    even = even[:len(odd)]
 
 TEST_SIZE = 0.1
 
-even_index = int((1 - TEST_SIZE) * len(even))
-odd_index = int((1 - TEST_SIZE) * len(odd))
-print("Even train:", len(even[:even_index]), " Odd train:", len(odd[:odd_index]))
-print("Even test:", len(even[even_index:]), " Test:", len(odd[odd_index:]))
-train_dataset = even[:even_index] + odd[:odd_index]
-test_dataset = even[even_index:] + odd[odd_index:]
+games_index = int((1 - TEST_SIZE) * len(games_dataset))
+
+train_dataset = games_dataset[:games_index]
+test_dataset = games_dataset[games_index:]
+print(len(train_dataset), len(test_dataset))
 
 shuffle(train_dataset)
 shuffle(test_dataset)
---
+++ dataset_split_even.py
@@ -0,0 +1,35 @@
+from random import shuffle
+
+from src.loading.data_loading import load_data_from_file
+from src.loading.data_loading import save_dataset_to_csv
+from src.patches import GAMES_DATASET_PATCH, TEST_DATASET_PATCH, TRAIN_DATASET_PATCH
+
+games_dataset = load_data_from_file(GAMES_DATASET_PATCH)
+even = []
+odd = []
+for fen, value in games_dataset:
+    if abs(value) <= 100:
+        even.append((fen, value))
+    else:
+        odd.append((fen, value))
+
+print("Even:", len(even), " Odd: ", len(odd))
+if len(even) < len(odd):
+    odd = odd[:len(even)]
+elif len(even) > len(odd):
+    even = even[:len(odd)]
+
+TEST_SIZE = 0.1
+
+even_index = int((1 - TEST_SIZE) * len(even))
+odd_index = int((1 - TEST_SIZE) * len(odd))
+print("Even train:", len(even[:even_index]), " Odd train:", len(odd[:odd_index]))
+print("Even test:", len(even[even_index:]), " Test:", len(odd[odd_index:]))
+train_dataset = even[:even_index] + odd[:odd_index]
+test_dataset = even[even_index:] + odd[odd_index:]
+
+shuffle(train_dataset)
+shuffle(test_dataset)
+
+save_dataset_to_csv(train_dataset, TRAIN_DATASET_PATCH)
+save_dataset_to_csv(test_dataset, TEST_DATASET_PATCH)
