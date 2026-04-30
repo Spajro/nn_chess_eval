fix nnue forward calculation

src/core.py
@@ -90,7 +90,7 @@ def iterate(data, model, criterion, accuracy):
     loss_sum = 0.0
     accuracy_sum = 0.0
     with torch.no_grad():
-        for batch, color, truth in data:
+        for batch, color,interpolation, truth in data:
             out = model.forward(batch, color).reshape(data.batch_size())
             loss = criterion(out, truth)
             accuracy_value = accuracy(out, truth).sum() / data.batch_size()

src/models/nnue.py
@@ -17,8 +17,13 @@ class NNUE(nn.Module):
                                         self.layer2,
                                         self.layer3)
 
-    def forward(self, x: (torch.Tensor, torch.Tensor), color: torch.Tensor):
-        color = color.reshape(-1, 1).long()
+    def forward(self, x: list[tuple[torch.Tensor, torch.Tensor]], color: torch.Tensor) -> torch.Tensor:
+        result = []
+        for i in range(0, len(x)):
+            result.append(self.fwd(x[i], color[i].item()))
+        return torch.stack(result)
+
+    def fwd(self, x: tuple[torch.Tensor, torch.Tensor], color: int) -> torch.Tensor:
         white = x[0].reshape(1, -1)
         black = x[1].reshape(1, -1)
 
@@ -29,4 +34,4 @@ class NNUE(nn.Module):
 
         relu1 = torch.clamp(accumulator, 0.0, 1.0)
         relu2 = torch.clamp(self.layer2(relu1), 0.0, 1.0)
-        return self.layer3(relu2)
+        return torch.clamp(self.layer3(relu2), 0.0, 1.0)

test_model.py
@@ -4,7 +4,7 @@ import chess
 import torch
 from torch import nn
 
-from src.loading.data_loading import load_dataset, wdl_to_cp
+from src.loading.data_loading import wdl_to_cp, load_dataset_with_stats
 from src.loading.data_loading_halfkp import features_to_tensor, board_to_feature_set
 from src.models.models import get_model
 from src.patches import TEST_DATASET_PATCH
@@ -18,12 +18,16 @@ def test(evaluate, data, ranges):
         results.append(0)
         counts.append(0)
 
-    for fen, val in data:
+    for fen, stats, val in data:
+        if val[0] == 'M':  # TODO
+            continue
+        else:
+            val = float(val)
         board = chess.Board(fen)
         r = evaluate(board)
         dif = abs(r - val)
 
-        out_of_range=True
+        out_of_range = True
         for i in range(len(ranges)):
             if ranges[i] > abs(val):
                 results[i] += dif
@@ -43,11 +47,11 @@ def log(ranges, results, counts):
 
     last_range = 0
     for i in range(len(ranges)):
-        if counts[i] ==0:
+        if counts[i] == 0:
             print(str(last_range) + ' - ' + str(ranges[i]), 0, counts[i])
         else:
             print(str(last_range) + ' - ' + str(ranges[i]), results[i] / counts[i], counts[i])
-        last_range= ranges[i]
+        last_range = ranges[i]
     if counts[len(ranges)] == 0:
         print(str(last_range) + ' - ' + 'inf', 0, counts[len(ranges)])
     else:
@@ -58,15 +62,15 @@ def evaluate_model(model: nn.Module, board: chess.Board, device: str):
     white_features, black_features = board_to_feature_set(board)
     white_tensor = features_to_tensor(white_features, device)
     black_tensor = features_to_tensor(black_features, device)
-    return model.forward((white_tensor, black_tensor), torch.tensor(board.turn))
+    return model.forward([(white_tensor, black_tensor)], torch.tensor(board.turn).reshape(1, -1))
 
 
 def eval_fen(model, fen: str, device):
     white_features, black_features = board_to_feature_set(chess.Board(fen))
     white_tensor = features_to_tensor(white_features, device).reshape(1, -1)
     black_tensor = features_to_tensor(black_features, device).reshape(1, -1)
-    color = torch.tensor(chess.Board(fen).turn).to(device)
-    return wdl_to_cp(model.forward((white_tensor, black_tensor), color)).item()
+    color = torch.tensor(chess.Board(fen).turn).to(device).reshape(1, -1)
+    return wdl_to_cp(model.forward([(white_tensor, black_tensor)], color)).item()
 
 
 parser = argparse.ArgumentParser(description='Halfkp NNUE test')
@@ -82,25 +86,27 @@ device = args.device
 checkpoint = torch.load(name)
 model = get_model(model_name).to(device)
 model.load_state_dict(checkpoint['model'])
-data = load_dataset(TEST_DATASET_PATCH)
+data = load_dataset_with_stats(TEST_DATASET_PATCH)
 torch.set_printoptions(sci_mode=False)
 
 fen1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 fen2 = "rnbqkbnr/pppppppp/8/8/2P5/8/PP1PPPPP/RNBQKBNR b KQkq - 0 1"
 fen3 = "rnbqkbnr/pp1ppppp/2p5/8/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 0 1"
 fen4 = "rnbqkbnr/pp1ppppp/2p5/8/2PP4/8/PP2PPPP/RNBQKBNR b KQkq - 0 1"
+fen5 = "8/6p1/p3Q1kp/1P1p2Bq/1p6/5N2/1PP2PPP/R4RK1 b - - 0 26"
 print("BENCHMARKS:")
 print(eval_fen(model, fen1, device), fen1)
 print(eval_fen(model, fen2, device), fen2)
 print(eval_fen(model, fen3, device), fen3)
 print(eval_fen(model, fen4, device), fen4)
+print(eval_fen(model, fen5, device), fen5)
 
-rngs = [100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500]
+rngs = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]
 
 print("RDZAWA BESTIA")
-r1,c1=test(evaluate, data,rngs)
-log(rngs,r1,c1 )
+r1, c1 = test(evaluate, data, rngs)
+log(rngs, r1, c1)
 
 print("CHECKPOINT MODEL")
-r2,c2=test(lambda x: wdl_to_cp(evaluate_model(model, x, device)).item(), data,rngs)
-log(rngs,r2,c2)
+r2, c2 = test(lambda x: wdl_to_cp(evaluate_model(model, x, device)).item(), data, rngs)
+log(rngs, r2, c2)